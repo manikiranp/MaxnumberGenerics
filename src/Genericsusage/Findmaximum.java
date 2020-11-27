fb2dec46f4d5a5c412fb5d2541a96707fba9b123
manikiranp <manikiranp3@gmail.com>
2020-11-27 21:01:17 +0530

    Finding maximum of given three numbers

---
+++ src/Genericsusage/Findmaximum.java
@@ -0,0 +1,37 @@
+package Genericsusage;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+
+
+public class Findmaximum {
+	private static Scanner inp= new Scanner(System.in);
+	
+
+	public static void main(String[] args) {
+		Integer[] intarray= new Integer[3];
+		System.out.println("Enter Three Numbers:");
+		for (int i=0;i<intarray.length;i++) {
+		intarray[i]= inp.nextInt(); 
+		}
+		Printmax(intarray);
+			
+		}
+
+
+	private static void Printmax(Integer[] intarray) {
+		List<Integer> intstream = Arrays.asList(intarray);
+		
+		Integer maximum=intstream.stream().max(Comparator.comparing(Integer::intValue)).orElse(null);
+		
+		System.out.println("maximum number:"+maximum);
+		
+	}
+	
+
+}
