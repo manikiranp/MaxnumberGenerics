2ebc207f43d02ad7f50a08209660c3c576d69f9a
manikiranp <manikiranp3@gmail.com>
2020-11-27 21:07:41 +0530

    Finding maximum using Streams on array

--- src/Genericsusage/Findmaximum.java
+++ src/Genericsusage/Findmaximum.java
@@ -15,6 +15,25 @@ public class Findmaximum {
 
 	public static void main(String[] args) {
 		System.out.println("Finding maximum from input using generics");
-	}
+		Integer[] intarray= new Integer[3];
+		System.out.println("Enter Three Numbers:");
+		for (int i=0;i<intarray.length;i++) {
+		intarray[i]= inp.nextInt(); 
+		}
+		Printmax(intarray);
+			
+		}
+
+
+		private static void Printmax(Integer[] intarray) {
+		List<Integer> intstream = Arrays.asList(intarray);
+
+		Integer maximum=intstream.stream().max(Comparator.comparing(Integer::intValue)).orElse(null);
+
+		System.out.println("maximum number:"+maximum);
+
+		}
+		
+	
 
 }
