feae8946c98aecb118556f1c8ca89aedbb51bf2a
manikiranp <manikiranp3@gmail.com>
2020-11-27 21:03:07 +0530

    Finding maximum of given three numbers

--- src/Genericsusage/Findmaximum.java
+++ src/Genericsusage/Findmaximum.java
@@ -14,24 +14,7 @@ public class Findmaximum {
 	
 
 	public static void main(String[] args) {
-		Integer[] intarray= new Integer[3];
-		System.out.println("Enter Three Numbers:");
-		for (int i=0;i<intarray.length;i++) {
-		intarray[i]= inp.nextInt(); 
-		}
-		Printmax(intarray);
-			
-		}
-
-
-	private static void Printmax(Integer[] intarray) {
-		List<Integer> intstream = Arrays.asList(intarray);
-		
-		Integer maximum=intstream.stream().max(Comparator.comparing(Integer::intValue)).orElse(null);
-		
-		System.out.println("maximum number:"+maximum);
-		
+		System.out.println("Finding maximum from input using generics");
 	}
-	
 
 }
