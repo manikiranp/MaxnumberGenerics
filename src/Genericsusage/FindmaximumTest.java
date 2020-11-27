1b6994011f1f5f7b577df2c8bbfbbc280de33d2b
manikiranp <manikiranp3@gmail.com>
2020-11-27 22:40:57 +0530

    Junit test case passed for maximum at second position

--- src/Genericsusage/FindmaximumTest.java
+++ src/Genericsusage/FindmaximumTest.java
@@ -3,17 +3,23 @@ package Genericsusage;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.function.Predicate;
 
 public class FindmaximumTest {
 	Findmaximum validation = new Findmaximum();
 
 	@Test
 	public  void Printmax_at_firstposition() {
-		
 		Integer[] intarray= {966,84,90};
 		int	result=validation.Printmax(intarray);
 		Assert.assertEquals(966, result);
+	}
+		@Test
+	public  void Printmax_at_secondposition() {	
+		Integer[] intarray= {121,123,90};
+		int	result=validation.Printmax(intarray);
+		Assert.assertEquals(123, result);
+
+	
 			
 		
 	}
